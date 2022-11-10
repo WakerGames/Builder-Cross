2d9502074b485f1ce0aabc707b0a24354ed0024b
Emir Albayrak <61053198+MEmirAlbayrak@users.noreply.github.com>
2022-11-10 15:30:28 +0300

    movement fix , player mass decreased

--- Assets/Joystick Pack/Scripts/Base/Joystick.cs
+++ Assets/Joystick Pack/Scripts/Base/Joystick.cs
@@ -48,7 +48,7 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
         canvas = GetComponentInParent<Canvas>();
         if (canvas == null)
             Debug.LogError("The Joystick is not placed inside a canvas");
-
+        cam = canvas.worldCamera;
         Vector2 center = new Vector2(0.5f, 0.5f);
         background.pivot = center;
         handle.anchorMin = center;
@@ -64,9 +64,9 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
 
     public void OnDrag(PointerEventData eventData)
     {
-        cam = null;
-        if (canvas.renderMode == RenderMode.ScreenSpaceCamera)
-            cam = canvas.worldCamera;
+        //cam = null;
+       
+           
 
         Vector2 position = RectTransformUtility.WorldToScreenPoint(cam, background.position);
         Vector2 radius = background.sizeDelta / 2;
