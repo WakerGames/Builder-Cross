aec9e7158f80542e5e711c0805bce07279dc42f6
Emir Albayrak <61053198+MEmirAlbayrak@users.noreply.github.com>
2022-12-20 18:01:45 +0300

    Level 1 yaptım dokunmayın havalı sonu var

---
+++ Assets/Scripts/MainMenu/BunkerEnter.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunkerEnter : MonoBehaviour
+{
+    [SerializeField] GameObject _gameHUD;
+    [SerializeField] GameObject _joystick;
+    [SerializeField] GameObject _survivedScene;
+    [SerializeField] GameObject _tryAgainSurvivedScene;
+    [SerializeField] GameObject _mainCharacter;
+    [SerializeField] GameObject _mainCam;
+    [SerializeField] GameObject _bunkerCam;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //_mainCharacter.transform.eulerAngles = new Vector3(0f,0f,0f);
+            _bunkerCam.SetActive(true);
+            _mainCam.SetActive(false);
+            _mainCharacter.GetComponent<Player>().characterMoveSpeed = 2;
+            _gameHUD.SetActive(false);
+            _joystick.SetActive(false);
+            if (_mainCharacter.GetComponent<BoxManager>().BoxesOnHand.Count == 3)
+            {
+                _survivedScene.SetActive(true);
+            }
+            else if (_mainCharacter.GetComponent<BoxManager>().BoxesOnHand.Count == 0)
+            {
+                _tryAgainSurvivedScene.SetActive(true);
+                _mainCharacter.GetComponent<AnimatorController>().WalkMain();
+            }
+            else
+            { 
+                _tryAgainSurvivedScene.SetActive(true);
+            }
+        }
+    }
+
+}
