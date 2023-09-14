4c116b7ac0e9592ebc1ca3719b871047d4e7dd0d
nasir41 <iamnasir786@gmail.com>
2023-09-14 16:43:20 +0500

    utmp settings updated
    utmp settings updated

--- .utmp/RelWithDebInfo/303h2q4g/prefab/armeabi-v7a/prefab-configure/lib/arm-linux-androideabi/cmake/game-activity/game-activityConfig.cmake
+++ .utmp/RelWithDebInfo/303h2q4g/prefab/armeabi-v7a/prefab-configure/lib/arm-linux-androideabi/cmake/game-activity/game-activityConfig.cmake
@@ -1,8 +1,8 @@
 if(NOT TARGET game-activity::game-activity)
 add_library(game-activity::game-activity STATIC IMPORTED)
 set_target_properties(game-activity::game-activity PROPERTIES
-    IMPORTED_LOCATION "/Users/nasir41/.gradle/caches/transforms-3/6c263f9b274272e8756b525bc7d98ffe/transformed/jetified-games-activity-2.0.2/prefab/modules/game-activity/libs/android.armeabi-v7a/libgame-activity.a"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/nasir41/.gradle/caches/transforms-3/6c263f9b274272e8756b525bc7d98ffe/transformed/jetified-games-activity-2.0.2/prefab/modules/game-activity/include"
+    IMPORTED_LOCATION "/Users/nasir41/.gradle/caches/transforms-3/433d5246142d5dcc6d7e9ecc2f62bb01/transformed/jetified-games-activity-2.0.2/prefab/modules/game-activity/libs/android.armeabi-v7a/libgame-activity.a"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/nasir41/.gradle/caches/transforms-3/433d5246142d5dcc6d7e9ecc2f62bb01/transformed/jetified-games-activity-2.0.2/prefab/modules/game-activity/include"
     INTERFACE_LINK_LIBRARIES ""
 )
 endif()
@@ -10,8 +10,8 @@ endif()
 if(NOT TARGET game-activity::game-activity_static)
 add_library(game-activity::game-activity_static STATIC IMPORTED)
 set_target_properties(game-activity::game-activity_static PROPERTIES
-    IMPORTED_LOCATION "/Users/nasir41/.gradle/caches/transforms-3/6c263f9b274272e8756b525bc7d98ffe/transformed/jetified-games-activity-2.0.2/prefab/modules/game-activity_static/libs/android.armeabi-v7a/libgame-activity_static.a"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/nasir41/.gradle/caches/transforms-3/6c263f9b274272e8756b525bc7d98ffe/transformed/jetified-games-activity-2.0.2/prefab/modules/game-activity_static/include"
+    IMPORTED_LOCATION "/Users/nasir41/.gradle/caches/transforms-3/433d5246142d5dcc6d7e9ecc2f62bb01/transformed/jetified-games-activity-2.0.2/prefab/modules/game-activity_static/libs/android.armeabi-v7a/libgame-activity_static.a"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/nasir41/.gradle/caches/transforms-3/433d5246142d5dcc6d7e9ecc2f62bb01/transformed/jetified-games-activity-2.0.2/prefab/modules/game-activity_static/include"
     INTERFACE_LINK_LIBRARIES ""
 )
 endif()
